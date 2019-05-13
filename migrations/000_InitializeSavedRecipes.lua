54a9848c884bb3e12c6c62b720933606ac6558fb
Matt Parish <matt.p@wibbu.com>
2019-05-13 11:43:46 -0400

    Updating for Factorio version 0.17.x
    Updated documentation and versioning info to enable 0.17.x compatibility.

--- migrations/000_InitializeSavedRecipes.lua
+++ migrations/000_InitializeSavedRecipes.lua
@@ -1,18 +1,18 @@
 for index, force in pairs(game.forces) do
-    local technologies = force.technologies
-    local recipes = force.recipes
-  
-    recipes["small-street-lamp"].enabled = technologies["optics"].researched
+  local technologies = force.technologies
+  local recipes = force.recipes
 
-    local energyDistro1Enabled = technologies["electric-energy-distribution-1"].researched
-    recipes["medium-street-lamp"].enabled = energyDistro1Enabled
-    recipes["big-street-lamp"].enabled = energyDistro1Enabled
+  recipes["small-street-lamp"].enabled = technologies["optics"].researched
 
-    recipes["lighted-substation"].enabled = technologies["electric-energy-distribution-2"].researched
-  
-    if technologies["tanks"].researched then
-      recipes["explosive-cannon-shell"].enabled = true
-      recipes["cannon-shell"].reload()
-      recipes["explosive-cannon-shell"].reload()
-    end
+  local energyDistro1Enabled = technologies["electric-energy-distribution-1"].researched
+  recipes["medium-street-lamp"].enabled = energyDistro1Enabled
+  recipes["big-street-lamp"].enabled = energyDistro1Enabled
+
+  recipes["lighted-substation"].enabled = technologies["electric-energy-distribution-2"].researched
+
+  if technologies["tanks"].researched then
+    recipes["explosive-cannon-shell"].enabled = true
+    recipes["cannon-shell"].reload()
+    recipes["explosive-cannon-shell"].reload()
   end
+end
